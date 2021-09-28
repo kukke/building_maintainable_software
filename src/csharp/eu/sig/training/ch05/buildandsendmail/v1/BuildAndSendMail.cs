771d7f980a0d6a4907a97a4c165b646ff9e347ab
Christian Vestergaard <CHV@jsworldmedia.com>
2021-09-28 13:35:48 +0200

    asdf

--- src/csharp/eu/sig/training/ch05/buildandsendmail/v1/BuildAndSendMail.cs
+++ src/csharp/eu/sig/training/ch05/buildandsendmail/v1/BuildAndSendMail.cs
@@ -1,20 +1,39 @@
 ﻿namespace eu.sig.training.ch05.buildandsendmail.v1
 {
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+	}
+
+    public class MailContent
+    {
+        public string Subject { get; set; }
+        public string[] Messages { get; set; }
+
+        public string AllMessages => Messages[0] + Messages[1] + Messages[2];
+	}
+
+    public class BuildAndSendMailParameters
+    {
+        public Person Receiver { get; set; }
+        public MailContent MailContent { get; set; }
+
+	}
+
     public class BuildAndSendMail
     {
         // tag::buildAndSendMail[]
-        public void DoBuildAndSendMail(MailMan m, string firstName, string lastName,
-            string division, string subject, MailFont font, string message1,
-            string message2, string message3)
+        public void DoBuildAndSendMail(MailMan m, BuildAndSendMailParameters mailparams, MailFont font, string division)
         {
             // Format the email address
-            string mId = $"{firstName[0]}.{lastName.Substring(0, 7)}" +
+            string mId = $"{mailparams.Receiver.FirstName[0]}.{mailparams.Receiver.LastName.Substring(0, 7)}" +
                 $"@{division.Substring(0, 5)}.compa.ny";
             // Format the message given the content type and raw message
             MailMessage mMessage = FormatMessage(font,
-                message1 + message2 + message3);
+                mailparams.MailContent.AllMessages);
             // Send message
-            m.Send(mId, subject, mMessage);
+            m.Send(mId, mailparams.MailContent.Subject, mMessage);
         }
         // end::buildAndSendMail[]
 
